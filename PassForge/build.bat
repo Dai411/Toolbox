195118f4fe68d1a464a409abdd10693ed0af8acb
Rock411 <yangln@126.com>
2025-07-28 19:50:10 +0200

    Update build.bat

--- PassForge/build.bat
+++ PassForge/build.bat
@@ -1,5 +1,5 @@
 @echo off
 echo [*] Building PassForge.exe ...
-pyinstaller --onefile --noconsole --icon=icon.ico PassForge.py
+pyinstaller --onefile --noconsole --clean --strip --icon=icon.ico PassForge.py
 echo [✓] Done. Check dist folder.
 pause
